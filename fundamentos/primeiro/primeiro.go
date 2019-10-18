30f320459dba9da73ff076a96cf3fb17f985b9fe
Ricart <antonio.ricart@academico.ifpb.edu.br>
2019-10-18 14:57:59 -0300

    criando o primeiro programa Go

---
+++ fundamentos/primeiro/primeiro.go
@@ -0,0 +1,14 @@
+//Programas executáveis em Go iniciam pelo pecote main
+package main
+
+/*
+Os códigos em Go são organizados em pacotes
+e para usá-los é necessário declarar, um ou vários imports
+*/
+
+import "fmt"
+
+//A função main é a porta de entrada de um programa go
+func main() {
+	fmt.Println("Olá mundo")
+}
