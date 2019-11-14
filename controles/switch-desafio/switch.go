1d97aaa792b6ce80961265906e427e0725fd9d89
Ricart <ricarteusuo11@gmail.com>
2019-11-14 12:29:00 -0300

    diferentes formas de se usar o switch

---
+++ controles/switch-desafio/switch.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func notaParaConceito(nota float64) string {
+	switch {
+	case nota >= 9 && nota <= 10:
+		return "A"
+	case nota >= 8 && nota < 9:
+		return "B"
+	case nota >= 5 && nota < 8:
+		return "C"
+	case nota >= 3 && nota < 5:
+		return "D"
+	default:
+		return "E"
+	}
+}
+
+func main() {
+	fmt.Println(notaParaConceito(9.8))
+	fmt.Println(notaParaConceito(6.9))
+	fmt.Println(notaParaConceito(2.1))
+}
