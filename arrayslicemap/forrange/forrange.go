b549fcbfae6a4035e5dd3e58ba5bb009708e25b3
Ricart <ricarteusuo11@gmail.com>
2019-11-21 15:07:30 -0300

    percorrendo arrays com for aprimorado

---
+++ arrayslicemap/forrange/forrange.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	numeros := [...]int{1, 2, 3, 4, 5} // compilador conta!
+
+	for i, numero := range numeros { // acessa o indice e o valor associado
+		fmt.Printf("%d) %d\n", i, numero)
+	}
+
+	for i := range numeros { // pegando apenas o indice
+		fmt.Println(i)
+	}
+
+	for _, num := range numeros { // ignorando o indice
+		fmt.Println(num)
+	}
+
+}
