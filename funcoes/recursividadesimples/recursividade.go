ffd78f4c856f91ce493b9948f29d3503e397e19b
Ricart <ricarteusuo11@gmail.com>
2020-02-23 20:36:49 -0300

    adicionando exemplos de recursividade

---
+++ funcoes/recursividadesimples/recursividade.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func fatorial(n uint) uint {
+	switch {
+	case n == 0:
+		return 1
+	default:
+		return n * fatorial(n-1)
+	}
+}
+
+func main() {
+	resultado := fatorial(5)
+	fmt.Println(resultado)
+
+	// resultado2 := fatorial(-2)
+	// fmt.Println(resultado2)
+}
