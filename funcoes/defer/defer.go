9eef88528bdae6de159a94ad3db3c5505e31fb4a
Ricart <ricarteusuo11@gmail.com>
2020-02-23 20:37:14 -0300

    aplicando o método defer em um exemplo

---
+++ funcoes/defer/defer.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func obterValorAprovado(numero int) int {
+	defer fmt.Println("Fim")
+	if numero > 5000 {
+		fmt.Println("Valor alto")
+		return 5000
+	}
+	fmt.Println("Valor baixo")
+	return numero
+}
+
+func main() {
+	fmt.Println(obterValorAprovado(100000))
+}
