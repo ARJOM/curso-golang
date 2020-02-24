f34de1305664620a0c7ac37ca23675cee4be0808
Ricart <ricarteusuo11@gmail.com>
2020-02-24 19:27:10 -0300

    aplicando conceito de interface com estruturas em go

---
+++ tipos/interface1/interface.go
@@ -0,0 +1,45 @@
+package main
+
+import "fmt"
+
+type imprimivel interface {
+	toString() string
+}
+
+type pessoa struct {
+	nome      string
+	sobrenome string
+}
+
+type produto struct {
+	nome  string
+	preco float64
+}
+
+// interfaces são implementadas implicitamente
+
+func (p pessoa) toString() string {
+	return p.nome + " " + p.sobrenome
+}
+
+func (p produto) toString() string {
+	return fmt.Sprintf("%s - R$ %.2f", p.nome, p.preco)
+}
+
+func imprimir(x imprimivel) {
+	fmt.Println(x.toString())
+}
+
+func main() {
+	var coisa imprimivel = pessoa{"Roberto", "Silva"}
+	fmt.Println(coisa.toString())
+	imprimir(coisa)
+
+	coisa = produto{"Chinelo", 4.5}
+	fmt.Println(coisa.toString())
+	imprimir(coisa)
+
+	produto := produto{"Calça", 44.99}
+	fmt.Println(produto.toString())
+	imprimir(produto)
+}
