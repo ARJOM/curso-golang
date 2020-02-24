ac8a25c86d1005a06a24c9cdc271db148c9accea
Ricart <ricarteusuo11@gmail.com>
2020-02-24 18:45:20 -0300

    aplicando metodos a estruturas em go

---
+++ tipos/metodos/metodos.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+type pessoa struct {
+	nome      string
+	sobrenome string
+}
+
+func (p pessoa) getNomeCompleto() string {
+	return p.nome + " " + p.sobrenome
+}
+
+// Quando necessário aterar as informações de um struct, se usa ponteiro no receiver
+func (p *pessoa) setNomeCompleto(nomeCompleto string) {
+	partes := strings.Split(nomeCompleto, " ")
+	p.nome = partes[0]
+	p.sobrenome = partes[1]
+}
+
+func main() {
+	p1 := pessoa{"Pedro", "Silva"}
+	fmt.Println(p1.getNomeCompleto())
+
+	p1.setNomeCompleto("Maria Pereira")
+	fmt.Println(p1.getNomeCompleto())
+}
