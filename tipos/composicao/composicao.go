67995400fee90c531f2c8e9933a2cdcb8fc2f5fa
Ricart <ricarteusuo11@gmail.com>
2020-02-24 19:47:09 -0300

    aplicando composição a interfaces go

---
+++ tipos/composicao/composicao.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+type esportivo interface {
+	ligarTurbo()
+}
+
+type luxuoso interface {
+	fazerBaliza()
+}
+
+type esportivoLuxuoso interface {
+	esportivo
+	luxuoso
+	// pode adicionar mais metodos
+}
+
+type bmw7 struct{}
+
+func (b bmw7) ligarTurbo() {
+	fmt.Println("Turbo...")
+}
+
+func (b bmw7) fazerBaliza() {
+	fmt.Println("Baliza...")
+}
+
+func main() {
+	var b esportivoLuxuoso = bmw7{}
+	b.fazerBaliza()
+	b.ligarTurbo()
+}
