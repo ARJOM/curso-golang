9b391d89d850d9ce7a78eb38093d6a3aa9b8eafe
Ricart <ricarteusuo11@gmail.com>
2020-02-24 19:33:59 -0300

    aplicando conceito de interfaces com métodos que alteram estruturas em go

---
+++ tipos/interface2/interface.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+type esportivo interface {
+	ligarTurbo()
+}
+
+type ferrari struct {
+	modelo          string
+	turboLigado     bool
+	velocidadeAtual int
+}
+
+func (f *ferrari) ligarTurbo() {
+	f.turboLigado = true
+}
+
+func main() {
+	carro1 := ferrari{"F40", false, 0}
+	carro1.ligarTurbo()
+
+	// Ao trabalhar em nível de interface é necessário atribuir o endereço da estrutura
+	var carro2 esportivo = &ferrari{"F40", false, 0}
+	carro2.ligarTurbo()
+
+	fmt.Println(carro1, carro2)
+}
