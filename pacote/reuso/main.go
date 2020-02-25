38a0f7257d89f2abfdfc72c8cbdec8ec549a2c04
Ricart <ricarteusuo11@gmail.com>
2020-02-25 11:39:21 -0300

    reusando código criado no workspace go

---
+++ pacote/reuso/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/ARJOM/area"
+)
+
+func main() {
+	fmt.Println(area.Circ(6.0))
+	fmt.Println(area.Rect(4, 5.4))
+	// fmt.Println(area.TrianguloEq(12, 6))
+}
