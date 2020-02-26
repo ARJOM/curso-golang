a2f5ac0fc3cc2f5b6d493f1039caedf37a14688e
Ricart <ricarteusuo11@gmail.com>
2020-02-26 14:11:50 -0300

    criando dataset para os testes

---
+++ testes/tabela/strings_test.go
@@ -0,0 +1,30 @@
+package strings
+
+import (
+	"strings"
+	"testing"
+)
+
+const msgIndex = "%s (parte: %s) - índices: esperado (%d) <> encontrado (%d)."
+
+func TestIndex(t *testing.T) {
+	testes := []struct {
+		texto    string
+		parte    string
+		esperado int
+	}{
+		{"ADS é massa", "ADS", 0},
+		{"", "", 0},
+		{"Opa", "opa", -1},
+		{"antonio", "o", 3},
+	}
+
+	for _, teste := range testes {
+		t.Logf("Massa: %v", teste)
+		atual := strings.Index(teste.texto, teste.parte)
+
+		if atual != teste.esperado {
+			t.Errorf(msgIndex, teste.texto, teste.parte, teste.esperado, atual)
+		}
+	}
+}
